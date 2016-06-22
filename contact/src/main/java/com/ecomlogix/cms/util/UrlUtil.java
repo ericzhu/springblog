77fc25d79e5746161dda6990ac51f5daaf4c3b8f
Eric Zhu <eric.zhu@esignlive.com>
2016-06-22 14:15:32 -0400

    add update functionality

--- contact/src/main/java/com/ecomlogix/cms/util/UrlUtil.java
+++ contact/src/main/java/com/ecomlogix/cms/util/UrlUtil.java
@@ -1,8 +1,25 @@
 package com.ecomlogix.cms.util;
 
+import org.springframework.web.util.UriUtils;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+
 /**
  * Created by eric on 19/06/2016.
  */
 public class UrlUtil {
+    public static String encodeUrlPath(String pathSegment, HttpServletRequest httpServletRequest) {
 
+        String encoding = httpServletRequest.getCharacterEncoding() == null ? WebUtils.DEFAULT_CHARACTER_ENCODING : httpServletRequest.getCharacterEncoding();
+
+        try {
+            pathSegment = UriUtils.encodePathSegment(pathSegment, encoding);
+        } catch (UnsupportedEncodingException e) {
+            //
+        }
+
+        return pathSegment;
+    }
 }
